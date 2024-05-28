f47d4ad03b9fc0923f7decdcb313eceb84a4484b
ammon.roberts@vivint.com <ammon.roberts@vivint.com>
2024-05-28 15:22:29 -0600

    Small API change.

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -6,14 +6,14 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-app.MapGet("/pause", async (HttpClient client) =>
+app.MapGet("/pause", async (uint minutes, HttpClient client) =>
 {
-	var response = await client.GetAsync($"http://{builder.Configuration.GetValue<string>("PiHoles:PiHole0:URL")}/admin/api.php?disable=60&auth={builder.Configuration.GetValue<string>("PiHoles:PiHole0:APIKey")}");
+	var response = await client.GetAsync($"http://{builder.Configuration.GetValue<string>("PiHoles:PiHole0:URL")}/admin/api.php?disable={minutes * 60}&auth={builder.Configuration.GetValue<string>("PiHoles:PiHole0:APIKey")}");
 
 	return response;
 });
 
-app.MapGet("/unpause", async (HttpClient client) =>
+app.MapPost("/unpause", async (HttpClient client) =>
 {
 	var response = await client.GetAsync($"http://{builder.Configuration.GetValue<string>("PiHoles:PiHole0:URL")}/admin/api.php?enable&auth={builder.Configuration.GetValue<string>("PiHoles:PiHole0:APIKey")}");
 
